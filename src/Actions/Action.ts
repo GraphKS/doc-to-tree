054987ef6ee99aec0b698ab7a2747730467cad78
pierre paci <pierre.paci@gmail.com>
2019-11-03 17:01:47 +0100

    add ability to run action

--- src/Actions/Action.ts
+++ src/Actions/Action.ts
@@ -4,22 +4,22 @@ interface ActionOptions {
     id: string
     description: string
     title: string
+    includeInExport: boolean
 }
 
-// Should action have a method to execute themselves or should they be agnostic and execution information all contained
-// in the runner ?
-// Maybe create Subtype like InteractiveActions to be runned by InteractiveRunner ?
-export class Action {
+export abstract class Action {
 
-    public id: string;
-    public description: string;
-    public title: string;
-    private _connections: Array<Action> = [];
+    public readonly id: string;
+    public readonly description: string;
+    public readonly title: string;
+    protected readonly _connections: Array<Action> = [];
+    public readonly includeInExport: boolean;
 
-    constructor({id, description, title}: ActionOptions) {
+    constructor({id, description, title, includeInExport = true}: ActionOptions) {
         this.id = id;
         this.description = description;
         this.title = title;
+        this.includeInExport = includeInExport;
     }
 
     public addConnection(action: Action) {
@@ -34,4 +34,14 @@ export class Action {
         node.addConnection(this);
         return this
     }
+
+    public run(ctx: Context): Context {
+        if (ctx.hasOwnProperty(this.id)) {
+            throw new Error(`ContextError. ${this.id} is already in context.`)
+        }
+        ctx[this.id] = this.executeWithinContext(ctx);
+        return ctx;
+    }
+
+    protected abstract executeWithinContext(ctx: Context): any
 }
