2dfad5cf361e1cf9d6d933e38c96c3baece82031
pierre paci <pierre.paci@gmail.com>
2019-11-05 10:28:57 +0100

    test for execution

--- tests/Procedure.test.ts
+++ tests/Procedure.test.ts
@@ -36,4 +36,21 @@ describe("Procedure", () => {
             });
         }).toThrow();
     });
+
+    test("it run trivial procedure", async () => {
+        const start = new PassiveAction({id: "start", title: "foo", includeInExport: true, description: ""});
+        const end = new PassiveAction({id: "end", title: "bar", includeInExport: true, description: ""});
+        end.connectAfter(start);
+        const procedure = new Procedure({
+            name: "myTestProcedure",
+            description: "",
+            authors: ["Pierre"],
+            creationTimestamp: Date.now(),
+            start: start,
+            ends: [end]
+        });
+        let ctx = await procedure.execute();
+        expect(ctx).toHaveProperty("start");
+        expect(ctx).toHaveProperty("end");
+    });
 });
