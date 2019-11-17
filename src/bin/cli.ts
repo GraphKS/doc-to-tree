1d8929c400c78b6afaeb788b6b430b2ba7910497
pierre paci <pierre.paci@gmail.com>
2019-11-17 11:09:06 +0100

    new CLI to generate documentation

---
+++ src/bin/cli.ts
@@ -0,0 +1,51 @@
+import {Command} from "commander";
+import {Procedure} from "../procedure";
+import {Exporter} from "../Exporter/Exporter";
+import {MarkdowExporter} from "../Exporter/Markdown/MarkdowExporter";
+import {DotExporter} from "../Exporter/Dot/DotExporter";
+import {resolve} from "path";
+
+const program = new Command("Logtopus");
+
+program
+    .command("generate <type> <procedure>")
+    .description("Generate an output of a specific type of the procedure.")
+    .option("-o, --output <output>", "output in a file. Like: './output/doc.md'")
+    .action(async (type: string, procedure: string, options: any) => {
+        await generate(type, procedure, options).catch(console.log);
+    });
+
+program.parse(process.argv);
+
+if (!program.args.length) program.help();
+
+async function generate(type: string, path: string, {output}: any) {
+    console.log(`Generating documentation for ${path}`);
+    const procedure = await Procedure.fromYaml(path);
+    let exporter: Exporter;
+    console.log(`Selected output type: ${type}`);
+    switch (type) {
+        case "markdown":
+            exporter = new MarkdowExporter(procedure);
+            break;
+        case "dot":
+            exporter = new DotExporter(procedure);
+            break;
+        default:
+            console.log("Invalid types");
+            console.log("Available types are: markdown, dot");
+            return;
+    }
+    if (!output) {
+        switch (type) {
+            case "markdown":
+                output = "doc.md";
+                break;
+            case "dot":
+                output = "doc.dot";
+                break;
+        }
+    }
+    exporter.exportToDisk(output);
+    console.log(`Documentation is now available in ${resolve(output)}`);
+}
